d70b2da201208b4fd0fd9ab81acdb64ef418810a
fran-veiras <franciscover99@gmail.com>
2023-03-09 20:59:00 -0300

    fix: cargo toml

---
+++ jast_lib/target/package/jast_lib-0.1.0/target/release/deps/itoa-ad3c8e6dc6855c51.d
@@ -0,0 +1,8 @@
+/home/francisco/Desktop/workspace/Jast/jast_lib/target/release/deps/itoa-ad3c8e6dc6855c51.rmeta: /home/francisco/.cargo/registry/src/github.com-1ecc6299db9ec823/itoa-1.0.6/src/lib.rs /home/francisco/.cargo/registry/src/github.com-1ecc6299db9ec823/itoa-1.0.6/src/udiv128.rs
+
+/home/francisco/Desktop/workspace/Jast/jast_lib/target/release/deps/libitoa-ad3c8e6dc6855c51.rlib: /home/francisco/.cargo/registry/src/github.com-1ecc6299db9ec823/itoa-1.0.6/src/lib.rs /home/francisco/.cargo/registry/src/github.com-1ecc6299db9ec823/itoa-1.0.6/src/udiv128.rs
+
+/home/francisco/Desktop/workspace/Jast/jast_lib/target/release/deps/itoa-ad3c8e6dc6855c51.d: /home/francisco/.cargo/registry/src/github.com-1ecc6299db9ec823/itoa-1.0.6/src/lib.rs /home/francisco/.cargo/registry/src/github.com-1ecc6299db9ec823/itoa-1.0.6/src/udiv128.rs
+
+/home/francisco/.cargo/registry/src/github.com-1ecc6299db9ec823/itoa-1.0.6/src/lib.rs:
+/home/francisco/.cargo/registry/src/github.com-1ecc6299db9ec823/itoa-1.0.6/src/udiv128.rs:
